fc4228df7beef616e5cb3db7df5cc6f15e2c1c93
Vincent Wu <vincent.wu@newsmed.fr>
2020-09-04 11:32:09 +0200

    Add link

--- src/components/site/Header.vue
+++ src/components/site/Header.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="bg-black flex justify-around">
-    <div class="text-white text-4xl">Composition API Movie App</div>
+    <div class="text-white text-4xl">
+      <a href="/">Composition API Movie App</a>
+    </div>
   </div>
 </template>
